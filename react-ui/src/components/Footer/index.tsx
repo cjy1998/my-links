07da03655e0e13e74c4859f0661b49ef736ace3f
cjy <1404340013@qq.com>
2025-08-01 18:04:05 +0800

    feat:添加logo

--- react-ui/src/components/Footer/index.tsx
+++ react-ui/src/components/Footer/index.tsx
@@ -10,23 +10,23 @@ const Footer: React.FC = () => {
       }}
       links={[
         {
-          key: 'Ant Design Pro',
-          title: 'Ant Design Pro',
+          key: 'DeepThinkAdmin',
+          title: 'DeepThinkAdmin',
           href: 'https://pro.ant.design',
-          blankTarget: true,
+          blankTarget: false,
         },
         {
           key: 'github',
           title: <GithubOutlined />,
-          href: 'https://github.com/ant-design/ant-design-pro',
+          href: 'https://github.com/cjy1998/my-links',
           blankTarget: true,
         },
-        {
-          key: 'Ant Design',
-          title: 'Ant Design',
-          href: 'https://ant.design',
-          blankTarget: true,
-        },
+        // {
+        //   key: 'Ant Design',
+        //   title: 'Ant Design',
+        //   href: 'https://ant.design',
+        //   blankTarget: false,
+        // },
       ]}
     />
   );
